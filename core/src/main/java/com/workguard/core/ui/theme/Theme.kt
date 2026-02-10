3c4ef907965ae0eaa0f3fa6557b6e1414e3b0af3
root <root@patrol.k3guard.cloud>
2026-02-10 15:50:11 +0000

    theme: set global typography to Poppins

--- core/src/main/java/com/workguard/core/ui/theme/Theme.kt
+++ core/src/main/java/com/workguard/core/ui/theme/Theme.kt
@@ -5,12 +5,21 @@ import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.workguard.core.R
 
 @Composable
 fun WorkGuardTheme(content: @Composable () -> Unit) {
+    val poppins = FontFamily(
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_semibold, FontWeight.SemiBold)
+    )
+
     MaterialTheme(
         colorScheme = lightColorScheme(),
-        typography = Typography(),
+        typography = Typography(defaultFontFamily = poppins),
         shapes = Shapes(),
         content = content
     )
