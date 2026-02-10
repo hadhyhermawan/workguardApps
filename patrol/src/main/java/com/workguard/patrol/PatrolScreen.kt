3f63f305cb8c4c3f432f8a562e7fe97247d81c9f
root <root@patrol.k3guard.cloud>
2026-02-10 13:36:24 +0000

    fix(patrol): bundle fonts in module and use patrol R

--- patrol/src/main/java/com/workguard/patrol/PatrolScreen.kt
+++ patrol/src/main/java/com/workguard/patrol/PatrolScreen.kt
@@ -56,7 +56,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.workguard.navigation.R
+import com.workguard.patrol.R
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
